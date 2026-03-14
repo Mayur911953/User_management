d98bc791e4bb327a3ef11784f7563138f5dca80c
Mayur911953 <mayurc2606@gmail.com>
2026-03-14 22:02:03 +0530

    add home endpoint

---
+++ src/main/java/com/example/usermanagement/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "User Management System API is running";
+    }
+}
